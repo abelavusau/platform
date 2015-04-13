b1867d3eb9be58cc645e74c3a90eece8b6a38662
Aliaksandr_Belavusau <Aliaksandr_Belavusau@epam.com>
2015-04-13 09:31:32 -0300

    deleted obsoleted files.

--- src/main/java/com/platform/web/configuration/core/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.platform.web.configuration.core;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
